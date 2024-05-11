0ae0d04461e28d8a8fb17abd3aef75c7203e8bbf
unknown <anderson.smachado@al.infnet.edu.br>
2024-05-11 17:37:49 -0300

    Estrutura inicial via Spring Initializr

---
+++ appDr1Tp1/src/main/java/br/anderson/infnet/appDr1Tp1/AppDr1Tp1Application.java
@@ -0,0 +1,13 @@
+package br.anderson.infnet.appDr1Tp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDr1Tp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDr1Tp1Application.class, args);
+	}
+
+}
